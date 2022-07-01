06fbb113511e3eef36143a2dbeed595c55715610
Abdurrahman <Abdrhmn.gh@gmail.com>
2022-07-01 13:56:14 +0700

    edit gitignore for sendemail file

--- backend/controller/SendEmailNotif.js
+++ backend/controller/SendEmailNotif.js
@@ -39,10 +39,10 @@ export async function sendEmail(req, res) {
           text: "Pendaftaran warga telah dilakukan", // plain text body
           html: pesanEmailNotifikasi, // html body
         });
-      
+
         console.log("Message sent: %s", info.messageId);
         // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-      
+
         // Preview only available when sending through an Ethereal account
         console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
         // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
