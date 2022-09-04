7410cd4a48bf1aad1b194345df1a6bf0767ca8d6
Abdurrahman <Abdrhmn.gh@gmail.com>
2022-09-04 19:58:53 +0700

    add file senEmail.js

--- backend/controller/Masukan.js
+++ backend/controller/Masukan.js
@@ -1,49 +1,49 @@
 import Masukan from "../models/MasukanModel.js";
 
 export const getAllMasukan = async (req, res) => {
-    try{
-        const masukan = await Masukan.findAll();
-        res.json(masukan);
-    }catch(error){
-        res.json({ message: error.message })
-    }
-}
+	try {
+		const masukan = await Masukan.findAll();
+		res.json(masukan);
+	} catch (error) {
+		res.json({ message: error.message });
+	}
+};
 
 export const getMasukanByID = async (req, res) => {
-    try{
-        const masukan = await Masukan.findAll({
-            where: {
-                id_masukan: req.params.id
-            }
-        });
-        res.json(masukan[0]);
-    }catch(error){
-        res.json({ message: error.message })
-    }
-}
+	try {
+		const masukan = await Masukan.findAll({
+			where: {
+				id_masukan: req.params.id,
+			},
+		});
+		res.json(masukan[0]);
+	} catch (error) {
+		res.json({ message: error.message });
+	}
+};
 
 export const createMasukan = async (req, res) => {
-    try{
-        await Masukan.create(req.body);
-        res.json({
-            "message" : "Masukan berhasil dibuat!"
-        });
-    }catch(error){
-        res.json({ message: error.message })
-    }
-}
+	try {
+		await Masukan.create(req.body);
+		res.json({
+			message: "Masukan berhasil dibuat!",
+		});
+	} catch (error) {
+		res.json({ message: error.message });
+	}
+};
 
 export const deleteMasukan = async (req, res) => {
-    try{
-        await Masukan.destroy({
-            where: {
-                user_id: req.params.id
-            }
-        });
-        res.json({
-            "message" : "Masukan berhasil dihapus!"
-        });
-    }catch(error){
-        res.json({ message: error.message })
-    }
-}
+	try {
+		await Masukan.destroy({
+			where: {
+				user_id: req.params.id,
+			},
+		});
+		res.json({
+			message: "Masukan berhasil dihapus!",
+		});
+	} catch (error) {
+		res.json({ message: error.message });
+	}
+};
